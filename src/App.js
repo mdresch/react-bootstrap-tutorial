4cf4faa9cf158ae39f20bafd553294d65f78c8c0
mdresch <m.drescher@cbaconsultancy.nl>
2021-06-07 16:14:50 +0200

    2 files modified, 3 added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import './js/neumorphism.js';
+
 import Navbar from './partials/_navigation';
 import Head from './partials/_head';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
